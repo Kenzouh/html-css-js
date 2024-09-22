7679da06613dd6c252bb07dc2f49671fd86577b5
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-09-22 13:32:08 +0800

    JavaScript Ternary Operators.

---
+++ js/index12_ternaryOperators.js
@@ -0,0 +1,63 @@
+// ternary operator = shortcut to if and else statements.
+//                    helps to assign a variable based on a condition.
+
+//                    condition ? outputIfTrue : outputIfFalse;
+
+// Pro: More condensed. Easier to read than if-else statements.
+
+
+let online = true;
+
+              // Since Boolean, just put the Boolean variable itself in the condition.
+onlineStatus = online ? "You're online!" : "You're offline!";
+
+console.log("let online = true;");
+console.log("onlineStatus = online ? \"You're online!\" : \"You're offline!\";");
+console.log("");
+console.log(onlineStatus);
+
+console.log("");
+console.log("=========================");
+console.log("");
+
+// ==========================================================================================
+
+let close = false;
+
+//           condition ? outputIfTrue : outputIfFalse;
+websiteStatus = closed ? "Closed!" : "Open! :D";
+
+console.log("let close = false;");
+console.log("webStatus = closed ? \"Closed!\" : \"Open! :D\"");
+console.log("");
+console.log(websiteStatus);
+
+console.log("");
+console.log("=========================");
+console.log("");
+
+// ==========================================================================================
+
+let cuteness = 100;
+
+adorableness = cuteness >= 50 ? "You're sooo cute! ^-^" : "You're cute. :)";
+
+console.log("let cuteness = 100;");
+console.log("adorableness = cuteness >= 50 ? \"You're sooo cute! ^-^\" : \"You're cute. :)\";");
+console.log("");
+console.log(adorableness);
+
+console.log("");
+console.log("=========================");
+console.log("");
+
+// ==========================================================================================
+
+let age = 48;
+
+seniorDiscount = age >= 60 ? "You get a senior discount!" : "You're too young for a senior discount.";
+
+console.log("let age = 48;");
+console.log("seniorDiscount = age >= 60 ? \"You get a senior discount!\" : \"You're too young for a senior discount.\";");
+console.log("");
+console.log(seniorDiscount); 

ca9566f57f22c94f0bd8eb0499afba28fa9c43d3
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-10-17 16:08:54 +0800

    JavaScript While Loop.

---
+++ js/index19_whileLoop.js
@@ -0,0 +1,12 @@
+// while loop = repeat some code WHILE some condition is true.
+//              repeats a code INFINITELY WHILE the condition remains true.
+
+
+let faveAnimal = "";
+
+while(faveAnimal === ""){
+
+    faveAnimal = window.prompt("What's your favorite animal? :)");
+}
+
+console.log(`Your favorite animal is a ${faveAnimal}`);

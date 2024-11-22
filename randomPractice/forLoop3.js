81ed21fce7b0c53cf6a2168aaaff31fecc10a8b1
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-11-22 20:44:45 +0800

    For Loop 3 [JS]

---
+++ randomPractice/forLoop3.js
@@ -0,0 +1,39 @@
+// Incrementation
+
+console.log("Incrementation:");
+
+console.log("");
+
+console.log("for(let i = 5; i <= 10; i++)");
+for(let i = 5; i <= 10; i++){
+    console.log(`i = ${i}`)
+}
+
+console.log("");
+
+console.log("for(let i = 3; i <= 8; i++)");
+for(let i = 3; i <= 8; i++){
+    console.log(`i = ${i}`);
+}
+
+console.log("");
+console.log("============================");
+console.log("");
+
+// Decrementation
+
+console.log("Decrementation:");
+
+console.log("");
+
+console.log("for(let i = 10; i >= 5; i--)");
+for(let i = 10; i >= 5; i--){
+    console.log(`i = ${i}`);
+}
+
+console.log("");
+
+console.log("for(let i = 8; i >= 3; i--)");
+for(let i = 8; i >= 3; i--){
+    console.log(`i = ${i}`);
+}
